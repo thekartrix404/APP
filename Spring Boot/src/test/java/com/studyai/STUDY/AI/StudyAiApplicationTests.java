20250040064455b5ae2fd7103fa489477a6e76d8
Karthik <Karthikkumaran1621@gmail.com>
2026-08-15 15:44:52 +0530

    spring boot setup

---
+++ Spring Boot/src/test/java/com/studyai/STUDY/AI/StudyAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyai.STUDY.AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
